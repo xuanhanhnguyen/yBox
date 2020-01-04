cdab085e0c03d80e0fb210100932327f671782e4
Nguyễn Xuân Hạnh <48309752+xuanhanhnguyen@users.noreply.github.com>
2020-01-04 23:22:03 +0700

    update layout

--- resources/views/page/layout/content.blade.php
+++ resources/views/page/layout/content.blade.php
@@ -1,8 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('page.layout.header')
+<head>
+    <meta charset="UTF-8">
+    <title>yBox - @yield('title')</title>
+    <base href="{{asset('')}}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/line-awesome.css">
+    <link rel="stylesheet" type="text/css" href="css/line-awesome-font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" type="text/css" href="lib/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="lib/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+</head>
 <body>
-@yield('content')
+<div class="wrapper">
+    @include('page.layout.header')
+    @yield('content')
+</div><!--theme-layout end-->
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/popper.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
--- resources/views/page/layout/header.blade.php
+++ resources/views/page/layout/header.blade.php
@@ -1,21 +1,114 @@
-<head>
-    <meta charset="UTF-8">
-    <title>yBox - tuyển dụng</title>
-    <base href="{{asset('')}}">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content=""/>
-    <meta name="keywords" content=""/>
-
-    <link rel="stylesheet" type="text/css" href="css/animate.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/line-awesome.css">
-    <link rel="stylesheet" type="text/css" href="css/line-awesome-font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/jquery.mCustomScrollbar.min.css">
-    <link rel="stylesheet" type="text/css" href="lib/slick/slick.css">
-    <link rel="stylesheet" type="text/css" href="lib/slick/slick-theme.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/responsive.css">
-</head>
+<header>
+    <div class="container">
+        <div class="header-data">
+            <div class="logo">
+                <a href="index.html" title=""><img src="images/logo.png" alt=""></a>
+            </div><!--logo end-->
+            <div class="search-bar">
+                <form>
+                    <input type="text" name="search" placeholder="Search...">
+                    <button type="submit"><i class="la la-search"></i></button>
+                </form>
+            </div><!--search-bar end-->
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.html" title="">
+                            <span><img src="images/icon1.png" alt=""></span>
+                            Trang chủ
+                        </a>
+                    </li>
+                    {{-------------------}}
+                    <li>
+                        <a href="companies.html" title="">
+                            <span><img src="images/icon2.png" alt=""></span>
+                            Tuyển dụng
+                        </a>
+                    </li>
+                    {{-------------------}}
+                    <li>
+                        <a href="projects.html" title="">
+                            <span><img src="images/icon3.png" alt=""></span>
+                            Học bổng
+                        </a>
+                    </li>
+                    {{-------------------}}
+                    <li>
+                        <a href="#" title="" class="not-box-open">
+                            <span><img src="images/icon7.png" alt=""></span>
+                            Thông báo
+                        </a>
+                        <div class="notification-box">
+                            <div class="nt-title">
+                                <h4>Setting</h4>
+                                <a href="#" title="">Clear all</a>
+                            </div>
+                            <div class="nott-list">
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img1.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div><!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img2.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div><!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img3.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div><!--notification-info -->
+                                </div>
+                                <div class="notfication-details">
+                                    <div class="noty-user-img">
+                                        <img src="images/resources/ny-img2.png" alt="">
+                                    </div>
+                                    <div class="notification-info">
+                                        <h3><a href="#" title="">Jassica William</a> Comment on your project.
+                                        </h3>
+                                        <span>2 min ago</span>
+                                    </div><!--notification-info -->
+                                </div>
+                                <div class="view-all-nots">
+                                    <a href="#" title="">View All Notification</a>
+                                </div>
+                            </div><!--nott-list end-->
+                        </div><!--notification-box end-->
+                    </li>
+                </ul>
+            </nav><!--nav end-->
+            <div class="menu-btn">
+                <a href="#" title=""><i class="fa fa-bars"></i></a>
+            </div><!--menu-btn end-->
+            <div class="user-account">
+                <div class="user-info">
+                    <img src="http://via.placeholder.com/30x30" alt="">
+                    <a>User</a>
+                    <i class="la la-sort-down"></i>
+                </div>
+                <div class="user-account-settingss">
+                    <ul class="us-links">
+                        <li><a href="#" title="">Trang cá nhân</a></li>
+                        <li><a href="#" title="">Đổi mật khẩu</a></li>
+                    </ul>
+                    <h3 class="tc"><a href="sign-in.html" title="">Đăng xuất</a></h3>
+                </div><!--user-account-settingss end-->
+            </div>
+        </div><!--header-data end-->
+    </div>
+</header><!--header end-->
