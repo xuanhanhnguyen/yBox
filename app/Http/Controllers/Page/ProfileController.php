40be0fc95207b1469967d30c4a733ba8709ace52
Nguyễn Xuân Hạnh <48309752+xuanhanhnguyen@users.noreply.github.com>
2020-01-07 18:14:49 +0700

    bài share hiện trong profile

--- app/Http/Controllers/Page/ProfileController.php
+++ app/Http/Controllers/Page/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ProfileController extends Controller
@@ -17,10 +18,18 @@ class ProfileController extends Controller
 
     public function index()
     {
-        return view('page.profile');
+        $data = DB::select('SELECT p.id,p.title,p.content,p.image,p.total_coment,p.total_like,p.total_share,
+        p.total_view,p.description,s.content as \'content_share\',u.full_name,u.avatar,p.created_at,s.created_at as \'time\' 
+		FROM share_post s 
+        JOIN post p ON p.id = s.post_id 
+        JOIN users u ON u.id = p.create_by 
+        WHERE s.user_id = ?', [Auth::user()->id]);
+
+        return view('page.profile', ['data' => $data]);
     }
 
-    public function avatar(Request $request){
+    public function avatar(Request $request)
+    {
         $user = User::find(Auth::user()->id);
         if ($request->hasFile('avatar')) {
             $file = $request->file('avatar');
@@ -30,7 +39,7 @@ class ProfileController extends Controller
 
             $user->avatar = $Hinh;
             $user->save();
-        } else if($request->hasFile('image')){
+        } else if ($request->hasFile('image')) {
             $file = $request->file('image');
             $name = $file->getClientOriginalName();
             $Hinh = Str::random(4) . '--' . $name;
@@ -41,7 +50,9 @@ class ProfileController extends Controller
         }
         return redirect('page/profile');
     }
-    public function edit(Request $request){
+
+    public function edit(Request $request)
+    {
         $user = User::find(Auth::user()->id);
 
         $user->full_name = $request->fullname;
@@ -51,4 +62,5 @@ class ProfileController extends Controller
         $user->save();
         return redirect('page/profile');
     }
+
 }
